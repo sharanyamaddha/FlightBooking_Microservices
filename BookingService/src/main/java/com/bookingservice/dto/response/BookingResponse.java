c69615cf584430e17ea9ca2bdfe7fde55d933199
Sharanya Maddha <sharanyamaddha@gmail.com>
2025-12-02 01:27:27 +0530

    added booking dto request and responses

---
+++ BookingService/src/main/java/com/bookingservice/dto/response/BookingResponse.java
@@ -0,0 +1,28 @@
+package com.bookingservice.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.bookingservice.enums.BookingStatus;
+import com.bookingservice.enums.TripType;
+
+import lombok.Data;
+
+
+@Data
+public class BookingResponse {
+
+	 private String pnr;
+	    private String bookerEmailId;
+	    private BookingStatus status;
+	    private Double totalAmount;
+	    private LocalDateTime bookingDateTime;
+	    TripType tripType;
+
+
+	    private String airlineName;
+	    private String source;
+	    private String destination;
+
+	    private List<PassengerResponse> passengers;
+}
